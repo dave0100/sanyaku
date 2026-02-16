414e3792378754ef8477c4898de7f4d03c430eda
David Hansmann <hansmann.d@debitel.net>
2026-02-16 10:04:15 +0100

    data fetching

--- sanyaku.js
+++ sanyaku.js
@@ -13,7 +13,27 @@
         leftRikishi: rikishiList[0] || "",
         rightRikishi: rikishiList[1] || "",
         outputText: "Jikan desu",
+        matchLoading: false,
+        sanyakuRecords: sanyakuRikishi,
       };
+    },
+    methods: {
+      startMatch: async function() {
+        var leftRecord = this.sanyakuRecords.find(function(r) { return r.shikonaEn === this.leftRikishi; }.bind(this));
+        var rightRecord = this.sanyakuRecords.find(function(r) { return r.shikonaEn === this.rightRikishi; }.bind(this));
+        if (!leftRecord || !rightRecord) return;
+        var leftId = leftRecord.id;
+        var rightId = rightRecord.id;
+        this.matchLoading = true;
+        try {
+          await Promise.all([
+            window.loadRikishiData(leftId, rightId),
+            window.loadRikishiData(rightId, leftId),
+          ]);
+        } finally {
+          this.matchLoading = false;
+        }
+      },
     },
   }).mount("#app");
   document.getElementById("loading-overlay").remove();
